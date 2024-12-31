13e65c1ce1a973097c0ed96a7b64afdcfbf723c1
Joseph <josephkambaulaya2004@gmail.com>
2024-12-31 14:24:34 +0300

    complete Function for displaying the menu

--- ScientificCalculator.cpp
+++ ScientificCalculator.cpp
@@ -7,3 +7,11 @@ using namespace std;
 
 // Function for displaying the menu
 void displayMenu() {
+	cout << "\n=== Scientific Calculator ===" << endl;
+    cout << "1. Basic Arithmetic" << endl;
+    cout << "2. Trigonometric Functions" << endl;
+    cout << "3. Logarithmic Functions" << endl;
+    cout << "4. Exponential Functions" << endl;
+    cout << "5. Exit" << endl;
+    cout << "Enter your choice (1-5): ";
+}
